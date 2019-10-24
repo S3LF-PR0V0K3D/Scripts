bb15cf66a37261dc2882353a6de9e667afb9b8d2
S3LF-PR0V0K3D <56975419+S3LF-PR0V0K3D@users.noreply.github.com>
2019-10-24 13:59:00 -0400

    Update sentimento-cryptocoins-twitter.py
    word feeling analysis on twitter

--- sentimento-cryptocoins-twitter.py
+++ sentimento-cryptocoins-twitter.py
@@ -3,19 +3,17 @@
 """
 Script para buscar sentimento de alguma criptomoeda no twitter
 Created on Tue Aug 27 12:30:07 2019
-
-@author: Felipe Ssoares
 """
 
 from textblob import TextBlob as tb
 import tweepy
 import numpy as np
 
-consumer_key = 'jcbC0lKBQnqywKJHWjQ3N6X8H'
-consumer_secret = 'dlZYpzygUwQ3c3E5okrdkbJhpc7gtphph1vGjuVgHOptxpAMQm'
+consumer_key = 'YOUR-KEY'
+consumer_secret = 'YOUR-SECRETKEY'
 
-access_token = '1166341851265359872-qBf2W33SuZ5lhWceu4gskyub3wWHvz'
-access_token_secret = '9tFklOPejZvY5GjsKgFYUUy56bzcTJCTPtbKIdAeHJF46'
+access_token = 'YOUR-TOKEN'
+access_token_secret = 'YOUR-TOKEN'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -24,7 +22,6 @@ api = tweepy.API(auth)
 #Variável que irá armazenar todos os Tweets com 
 #a palavra escolhida na função search da API
 pesquisa = input(
-        '========= DataCrypto Analytics ========='
         '\n\nQual palavra deseja calcular o sentimento no Twitter?'
         '\n\nDigite aqui por favor:')
 public_tweets = api.search(pesquisa)
